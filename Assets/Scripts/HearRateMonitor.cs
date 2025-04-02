3670d0f7ad1d0ce46370f8ef6d9d216dfbc1c1e4
caden768 <caden768@gmail.com>
2025-04-02 10:27:23 -0400

    Making doubly sure everything was pushed

--- Assets/Scripts/HearRateMonitor.cs
+++ Assets/Scripts/HearRateMonitor.cs
@@ -5,26 +5,43 @@ using UnityEngine;
 public class HeartRateMonitor : MonoBehaviour
 {
     public float speed = 3f; // Speed of the movement
+   
     private float screenWidth;
+   
+    [Range(0, 1)]
+    public float beep;
+    public AnimationCurve HeartLine;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        // Calculate the screen width in world units
+        beep = 0;
+
         screenWidth = Camera.main.orthographicSize * Camera.main.aspect;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        // Move the game object to the right
-        transform.position += new Vector3(Vector2.right.x * speed * Time.deltaTime, 0, 0);
+        Vector2 newPosition = transform.position;
+        newPosition.x += speed * Time.deltaTime;
+        newPosition.y = HeartLine.Evaluate(beep) * 50;
+        transform.position = newPosition;
+        //transform.position += new Vector3(transform.position.x += speed * Time.deltaTime);
+        //transform.position += new Vector3(transform.position.y, HeartLine.Evaluate(beep));
 
-        // Check if the game object has reached the right edge of the screen
+
         if (transform.position.x > screenWidth)
         {
-            // Reset the position to the left edge
+            
             transform.position = new Vector2(-screenWidth, transform.position.y);
+        }
+
+        beep += Time.deltaTime;
+        if (beep > 1)
+        {
+            beep = 0;
         }
     }
 }
