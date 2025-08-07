4f411ad2f7f511788498fdb7a2c298372817077e
caden768 <caden768@gmail.com>
2025-08-07 17:42:05 -0400

    things are getting whiled...Changing update code relating to making the clock chime into a coroutine.

--- Assets/Scripts/CuckooClock.cs
+++ Assets/Scripts/CuckooClock.cs
@@ -8,22 +8,46 @@ public class CuckooClock : MonoBehaviour
 
     public float hourDuration = 4f;
 
+    public Chime chime;
+
     private float currentTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        chime = gameObject.GetComponentInChildren<Chime>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime += Time.deltaTime;
 
-        minuteHand.eulerAngles -= Vector3.forward * 360 * Time.deltaTime / hourDuration;
+        StartCoroutine(ClockUpdate());
 
-       
+    }
+
+    IEnumerator ClockUpdate()
+    {
+        while (currentTime < hourDuration)
+        {
 
 
+
+            currentTime += Time.deltaTime;
+
+            minuteHand.eulerAngles -= Vector3.forward * 360 * Time.deltaTime / hourDuration;
+
+            hourHand.eulerAngles -= Vector3.forward * 360 * Time.deltaTime / hourDuration / 12;
+
+            if (currentTime > hourDuration)
+            {
+                chime.PlayChime();
+
+                currentTime = 0f;
+
+            }
+
+            yield return null;
+
+        }
     }
 }
