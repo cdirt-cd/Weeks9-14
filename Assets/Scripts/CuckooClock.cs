3f0568386ec8090cc5c314dabdf5144716e402f8
caden768 <caden768@gmail.com>
2025-08-07 17:23:00 -0400

    Minute hand rotates 360 degrees every 4 seconds

--- Assets/Scripts/CuckooClock.cs
+++ Assets/Scripts/CuckooClock.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CuckooClock : MonoBehaviour
 {
+    public Transform hourHand, minuteHand;
+
+    public float hourDuration = 4f;
+
+    private float currentTime = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,12 @@ public class CuckooClock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime += Time.deltaTime;
+
+        minuteHand.eulerAngles -= Vector3.forward * 360 * Time.deltaTime / hourDuration;
+
+       
+
+
     }
 }
